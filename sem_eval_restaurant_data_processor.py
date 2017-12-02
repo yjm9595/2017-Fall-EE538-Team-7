7778cc3cc1f66bd5479318f0df7bfabd863b91d4
Elvin Kim <bonwho09@gmail.com>
2017-12-02 13:45:55 +0900

    [Add] Additional data word embedding

--- sem_eval_restaurant_data_processor.py
+++ sem_eval_restaurant_data_processor.py
@@ -5,7 +5,11 @@ from nltk.corpus import stopwords
 
 # Function
 def create_word2vec_model(size=100, window=2, min_count=1, workers=4, iter=100, sg=1):
-    input_token_data, output_label = xml_reader.load_data('data/ABSA16_Restaurants_Train_SB1_v2.xml')
+    input_token_data, output_label = xml_reader.load_data_for_task_3('data/ABSA16_Restaurants_Train_SB1_v2.xml')
+
+    with open("data/ALL_TRAIN_DATA.data") as f:
+        for line in f.readlines():
+            input_token_data.append(line.split())
 
     review_model = Word2Vec(input_token_data, size=size, min_count=min_count, window=window, workers=workers, iter=iter, sg=sg)
     review_model.init_sims(replace=True)
@@ -21,7 +25,8 @@ if __name__ == "__main__":
     print(model.most_similar("expensive"))
     print(model.most_similar("waiter"))
     print(model.most_similar("noon"))
-
-    create_model_file()
+    #
+    create_model_file("sem_eval_restaurant_model_add_data.vec")
+    print("FINISH!!!")
 
 
